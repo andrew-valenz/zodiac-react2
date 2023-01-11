e32ea2a1a37438fdab420e0b24a0ad75f0012312
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-11 00:38:13 -0800

    css working and signs rendering

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './Main.css';
 import ZodiacCard from '../ZodiacCard/ZodiacCard.js';
 import zodiac from '../../data.js';
+import background from '../../Background.png';
 
 export default function Main() {
   return (
-    <main style={{}}>
-      {zodiac.map((sign) => (
-        <ZodiacCard key={sign.id} {...sign} />
-      ))}
-      ;
+    <main style={{ background: `url("${background}")` }}>
+      <div className="zodiac-container">
+        {zodiac.map((sign) => (
+          <ZodiacCard key={sign.id} {...sign} />
+        ))}
+        ;
+      </div>
     </main>
   );
 }
